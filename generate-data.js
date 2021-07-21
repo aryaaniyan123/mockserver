9046132b2f5084e5f73c20f7b6a01a05c4b464ad
aryaaniyan123 <aryaaniyan108@gmail.com>
2021-07-21 14:38:57 +0530

    Update generate-data.js

--- generate-data.js
+++ generate-data.js
@@ -1,19 +1,26 @@
 var faker = require('faker');
 
-/*function generateData () {
+function generateData () {
   var messages = [];
   for (var id = 0; id < 10; id++) {
+  
+    
+    let color = faker.commerce.color();
+    let ticketId = faker.random.number({min: 1000, max: 9999});
+    let Date = faker.date.future();
+    let weekday = faker.date.weekday();
+
     
     messages.push({
       "id": id,
-      "date_sent": date,
-      "priority": priority,
-      "message": message,
-      "status": status
+      "ticketId": ticketId,
+      "Date": Date,
+      "weekday": weekday,
+      "color": color
     });
   }
 
   return {messages};
 }
 
-module.exports = generateData;*/
+module.exports = generateData;
